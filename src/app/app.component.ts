2204fb2cd3eef536de76c4a9f36abf578649542b
Vishnupatel <vcvanthala.5546@gmail.com>
2019-01-09 19:23:09 +0530

    095 Why would you Need Services

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+ accounts = [
+   {
+     name: 'Master Account',
+     status:'active'
+   },
+   {
+     name: 'Test Account',
+     status: 'inactive'
+   },
+   {
+     name: 'Hidden Account',
+     status: 'unknown'
+   }
+ ];
+
+ onAccountAdded(newAccount: {name: string, status: string}){
+    this.accounts.push(newAccount);
+ }
+
+ onStatusChanged(updateInfo: {id: number, newStatus: string}){
+    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+ }
+}
