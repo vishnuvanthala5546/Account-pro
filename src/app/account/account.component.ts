2750f7c71e49eaa42a004119ae6c23161f382f7b
Vishnupatel <vcvanthala.5546@gmail.com>
2019-01-10 16:46:52 +0530

    100 How many Instances of Service Should It Be

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -7,7 +7,7 @@ import {AccountsService} from '../accounts.service';
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.scss'],
-  providers: [LoggingService, AccountsService]
+  providers: [LoggingService]
 })
 export class AccountComponent implements OnInit {
   @Input() account: {name: string, status: string};
@@ -23,7 +23,6 @@ export class AccountComponent implements OnInit {
   }
 
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
 }
